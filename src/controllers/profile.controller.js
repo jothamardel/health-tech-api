c7b8d3504b21496e7f1fda59930556022bf87c00
jothamardel <ardelmbiplang@gmail.com>
2022-10-15 23:14:39 +0100

    chore: updating profile controller

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -15,33 +15,6 @@ async function httpCreateUserAndUpdateMedicalHistory(req, res) {
 
     const user = await Profile.findOneAndUpdate({ userId: req.body.userId }, {...req.body}, { upsert: true});
     await User.findOneAndUpdate({ _id: req.body.userId }, {is_active: true});
-    // const user = await User.findById({ _id: userId });
-    // if (!user) {
-    //   res.status(404).json({
-    //     message: "user not found",
-    //     status: "error",
-    //     success: false,
-    //   });
-    //   return;
-    // }
-    // const profile = await Profile.findOneAndUpdate(
-    //   { userId: userId },
-    //   { ...req.body }
-    // );
-    // const data = await Profile.find({});
-    // if (!profile) {
-    //   const newProfile = new Profile({
-    //     ...req.body,
-    //   });
-    //   await newProfile.save();
-      // res.status(201).json({
-      //   message: "successful",
-      //   status: "success",
-      //   success: true,
-      //   data: data,
-      // });
-      // return;
-    // }
     res.status(200).json({
       message: "successful",
       status: "success",
