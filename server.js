bb9cada50e9db164970f84ee27af5bec94df6afb
unknown <spontaneousmanji@gmail.com>
2022-10-15 02:01:44 +0100

    chore: setting up server on heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const dotenv = require("dotenv");
 const { app } = require("./app");
+const { mongoConnect } = require("./src/services/db");
 dotenv.config();
 
 const PORT = process.env.PORT ?? 5000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT ?? 5000;
 const server = http.createServer(app);
 
 const startServer = async function () {
+  await mongoConnect();
   server.listen(PORT, () => {
     console.log(`server is listening on port:::: ${PORT}`);
   });
