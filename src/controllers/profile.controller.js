addaa08a68e51e39e21eaf9c50ba7daa4b4786b9
jothamardel <ardelmbiplang@gmail.com>
2022-10-16 01:31:15 +0100

    chore: updating profile controller

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -14,16 +14,19 @@ async function httpCreateUserAndUpdateMedicalHistory(req, res) {
   try {
 
     const user = await Profile.findOneAndUpdate({ userId: req.body.userId }, {...req.body}, { upsert: true});
-    await User.findOneAndUpdate({ _id: req.body.userId }, {is_active: true});
+    const userData = await User.findOneAndUpdate({ _id: req.body.userId }, {is_active: true}, {upsert: true});
+    console.log(userData)
     res.status(200).json({
       message: "successful",
       status: "success",
       success: true,
       data: user,
+      userDetails: userData
     });
   } catch (error) {
     res.status(200).json({
-      message: ` catch ::: ${error.message}`,
+      message: "Unable to perform operation",
+      error: error.message,
       success: false,
       status: "error",
     });
